7a30d93acd5cd82ad0ab21a152cae0443350068f
LEEJINTAEK <eesskk9909@gmail.com>
2023-01-30 19:21:57 +0900

    Feat: 디자인 변경

--- js/common.js
+++ js/common.js
@@ -21,7 +21,7 @@ const thisYear = document.querySelector(".this-year");
 thisYear.textContent = new Date().getFullYear(); //textcontent-> 글자내용의 값을 알아내거나 지정
 
 //좋아요 임시 버튼
-const likeIcon = document.querySelectorAll(".menu-review button i");
+const likeIcon = document.querySelectorAll(".restaurant button i");
 
 //// 딱 하나만 선택하도록
 // likeIcon.forEach(function (x) {
