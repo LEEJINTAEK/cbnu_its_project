3de5e31766ea2e8d9e7921ee2df79dea269e8436
LEEJINTAEK <eesskk9909@gmail.com>
2023-02-02 00:54:36 +0900

    food 이미지 및 스타일 이미지 추가

--- js/youtube.js
+++ js/youtube.js
@@ -1,23 +1,24 @@
-var tag = document.createElement('script');
+var tag = document.createElement("script");
 
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 function onYouTubeIframeAPIReady() {
-  new YT.Player('player', { //# player X
-    videoId: 'An6LvWQuj_8',  //최초재생할 유투브영상 ID 
+  new YT.Player("player", {
+    //# player X
+    videoId: "7J8Ps3JxJq0", //최초재생할 유투브영상 ID
     playerVars: {
-        autoplay: true,
-        loop: true,        //반복재생유무
-        playlist: 'An6LvWQuj_8' //반복재생 유투브ID입력해야함
+      autoplay: true,
+      loop: true, //반복재생유무
+      playlist: "7J8Ps3JxJq0", //반복재생 유투브ID입력해야함
     },
     events: {
-        onReady: function(event) {  //매소드
-            event.target.mute() //음소거
-        }
-    }
+      onReady: function (event) {
+        //매소드
+        event.target.mute(); //음소거
+      },
+    },
   });
 }
